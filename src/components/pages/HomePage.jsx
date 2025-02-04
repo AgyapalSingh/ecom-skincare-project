bfe741f586675cdbfd27713f1b4cc93f1235a88f
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-02-04 14:47:46 +0530

    BestSellers Component created

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../Layout/Layout";
-import {
-  GET_COLLECTION_BY_ID,
-  GET_COLLECTIONS,
-  GET_PRODUCTS,
-} from "../../lib/shopify/queries";
+import { GET_COLLECTION_BY_ID, GET_PRODUCTS } from "../../lib/shopify/queries";
 import shopifyApi from "../../lib/shopify/shopifyApi";
 import UniqayaLoader from "../snippets/UniqayaLoader";
 import { useCart } from "../../context/CartContext"; // Import the custom hook
+import BestSeller from "../collections/BestSeller";
 
 const HomePage = () => {
   const [loading, setLoading] = useState(true);
@@ -32,8 +29,6 @@ const HomePage = () => {
       setLoading(false);
     }
   };
-
-
 
   // Fetch Best Seller collection by ID
   const fetchCollectionByID = async () => {
@@ -67,6 +62,8 @@ const HomePage = () => {
 
   return (
     <Layout title="Uniqaya Lifestyle | Shop Now">
+      {/* Best Seller Collection Section */}
+      <BestSeller />
       {/* All Products Section */}
       <section className="max-w-[1130px] mx-auto px-4 sm:px-6 lg:px-0 justify-self-center">
         <h2 className="text-2xl font-bold text-center my-8">All Products</h2>
@@ -79,9 +76,18 @@ const HomePage = () => {
                 className="w-[200px] aspect-square mx-auto"
               />
               <h3 className="mt-2 font-semibold">{node.title}</h3>
-              <p className="text-gray-600">Rs. {node.priceRange.minVariantPrice.amount}</p>
+              <p className="text-gray-600">
+                Rs. {node.priceRange.minVariantPrice.amount}
+              </p>
               <button
-                onClick={() => addToCart({ id: node.id, title: node.title, price: node.priceRange.minVariantPrice.amount , image: node.images.edges[0]?.node.src})}
+                onClick={() =>
+                  addToCart({
+                    id: node.id,
+                    title: node.title,
+                    price: node.priceRange.minVariantPrice.amount,
+                    image: node.images.edges[0]?.node.src,
+                  })
+                }
                 className="mt-2 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
               >
                 Add to Cart
@@ -92,32 +98,6 @@ const HomePage = () => {
       </section>
 
 
-      {/* Best Seller Collection Section */}
-      <section className="max-w-[1130px] mx-auto px-4 sm:px-6 lg:px-0 justify-self-center">
-        <h2 className="text-2xl font-bold text-center my-8">Best Seller</h2>
-        {selectCollection.length > 0 ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
-            {selectCollection.map(({ node }) => (
-              <div key={node.id} className="text-center border p-4 rounded-md">
-                <img
-                  src={node.featuredImage.url || "default-image.jpg"}
-                  alt={node.images?.edges[0]?.node?.altText || node.title}
-                  className="w-[200px] aspect-square mx-auto"
-                />
-                <h3 className="text-lg font-semibold">{node.title}</h3>
-                <button
-                  onClick={() => addToCart({ id: node.id, title: node.title, price: node.priceRange?.minVariantPrice?.amount , image: node.featuredImage.url})}
-                  className="mt-2 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
-                >
-                  Add to Cart
-                </button>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <p className="text-center text-gray-500">No best sellers available</p>
-        )}
-      </section>
     </Layout>
   );
 };
--- src/lib/shopify/queries.jsx
+++ src/lib/shopify/queries.jsx
@@ -76,6 +76,11 @@ export const GET_COLLECTION_BY_ID = `{
             id
             url
           }
+            priceRange {
+            minVariantPrice {
+              amount
+            }
+          }
         }
       }
     }
