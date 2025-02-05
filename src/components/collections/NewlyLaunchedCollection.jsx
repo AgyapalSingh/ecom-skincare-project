4a101f1f7344a81a6e04271451eeba0ea5208e18
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-02-05 17:51:09 +0530

    Newly launched Title Changed

--- src/components/collections/NewlyLaunchedCollection.jsx
+++ src/components/collections/NewlyLaunchedCollection.jsx
@@ -38,7 +38,7 @@ const NewlyLaunchedCollection = () => {
         <UniqayaLoader />
       ) : (
         <section className="max-w-[1130px] mx-auto px-4 sm:px-6 lg:px-0 justify-self-center">
-          <h2 className="text-2xl font-bold text-center my-8">Best Seller</h2>
+          <h2 className="text-2xl font-bold text-center my-8">Newly Launched</h2>
           {selectCollection.length > 0 ? (
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
               {selectCollection.map(({ node }) => (
@@ -52,7 +52,7 @@ const NewlyLaunchedCollection = () => {
             </div>
           ) : (
             <p className="text-center text-gray-500">
-              No best sellers available
+              No Newly Launched available
             </p>
           )}
         </section>
