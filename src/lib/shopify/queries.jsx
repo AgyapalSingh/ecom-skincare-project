759ae8cbcf7682e5ccbb480aa599857442957308
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-02-05 17:04:12 +0530

    Queries Updated

--- src/lib/shopify/queries.jsx
+++ src/lib/shopify/queries.jsx
@@ -150,25 +150,6 @@ export const GET_PRODUCT_BY_HANDLE = `
 
 // 5. QUERY TO FETCH PRODUCT FROM HANDLE
 // [ALSO USED TO CREATE DYNAMIC PRODUCT PAGE - BUT DIFFERENCE IS WHEN PRODUCT IS CLICKED FROM COLLECTION]
-// export const GET_PRODUCT_BY_HANDLE_FROM_COLLECTION = `
-//   query getProductByHandle($handle: String!) {
-//     productByHandle(handle: $handle) {
-//       id
-//       title
-//       description
-//       featuredImage {
-//         url
-//         altText
-//       }
-//       priceRange {
-//         minVariantPrice {
-//           amount
-//         }
-//       }
-//     }
-//   }
-// `;
-
 export const GET_PRODUCT_BY_HANDLE_FROM_COLLECTION = `
   query getProductByHandle($handle: String!) {
     productByHandle(handle: $handle) {
@@ -202,36 +183,6 @@ export const GET_PRODUCT_BY_HANDLE_FROM_COLLECTION = `
 
 // 6. QUERY TO FETCH A SPECIFIC COLLECTION BY COLLECTION HANDLE
 // [USED TO CREATE DYNAMIC COLLECTION PAGE FOR ALL COLLECTIONS WHEN THEY ARE CLICKED FROM ALL COLLECTION PAGE]
-// export const GET_COLLECTION_BY_HANDLE = `
-//   query GetCollectionByHandle($handle: String!) {
-//     collectionByHandle(handle: $handle) {
-//       id
-//       title
-//       handle
-//       description
-//       products(first: 20) {
-//         edges {
-//           node {
-//             id
-//             title
-//             handle
-//             featuredImage {
-//               url
-//               altText
-//             }
-//             priceRange {
-//               minVariantPrice {
-//                 amount
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `;
-
-
 export const GET_COLLECTION_BY_HANDLE = `
   query GetCollectionByHandle($handle: String!) {
     collectionByHandle(handle: $handle) {
