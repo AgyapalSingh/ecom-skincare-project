f914599fc46eff715bc93c2311a6a785d40bb9f6
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-02-07 11:24:30 +0530

    ImageSlider Compenent in Snippets CReated Using Swiper

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Layout from "../Layout/Layout";
 import AllProducts from "../collections/AllProducts";
 import NewlyLaunchedCollection from "../collections/NewlyLaunchedCollection";
+import ImageSlider from "../snippets/ImageSlider";
 
 const HomePage = () => {
   return (
     <Layout title="Home | Uniqaya Lifestyle">
+      <ImageSlider/>
       <NewlyLaunchedCollection/>
       <AllProducts />
     </Layout>
---
+++ src/components/snippets/ImageSlider.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
+const desktopImages = [
+  "https://cdn.shopify.com/s/files/1/0589/0192/1956/files/UNI-CLUB.jpg?v=1738669086",
+  "https://cdn.shopify.com/s/files/1/0589/0192/1956/files/VD-Sale-Desktop.jpg?v=1738405338",
+];
+
+const mobileImages = [
+  "https://cdn.shopify.com/s/files/1/0589/0192/1956/files/UNI-CLUB-M.jpg?v=1738669085",
+  "https://cdn.shopify.com/s/files/1/0589/0192/1956/files/VD-Sale-Mobile.jpg?v=1738405338",
+];
+
+const ImageSlider = () => {
+  const [images, setImages] = useState(desktopImages);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setImages(mobileImages);
+      } else {
+        setImages(desktopImages);
+      }
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <Swiper
+      spaceBetween={30}
+      centeredSlides={true}
+      loop={true}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Autoplay, Pagination, Navigation]}
+      className="mySwiper"
+    >
+      {images.map((image, index) => (
+        <SwiperSlide className="uniq-swiper-slide" key={index}>
+          <img src={image} alt={`Slide ${index + 1}`}  loading={index === 0 ? "eager" : "lazy"} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default ImageSlider;
