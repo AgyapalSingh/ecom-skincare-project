55a441600075b3e8d907dcc08e6af9676916d8cd
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-02-18 14:13:51 +0530

    ProductCardForCollections Updated

--- src/components/snippets/ProductCardForCollection.jsx
+++ src/components/snippets/ProductCardForCollection.jsx
@@ -1,45 +1,48 @@
-import React,  { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useCart } from "../../context/CartContext";
-import "./snippetsCss/ProductCardForCollections.css"
+import "./snippetsCss/ProductCardForCollections.css";
 
-const ProductCardForCollection = ({ product, collectionHandle}) => {
+const ProductCardForCollection = ({ product, collectionHandle }) => {
   const navigate = useNavigate();
   const { addToCart } = useCart();
-
 
   const variants = product.variants.edges.map((edge) => edge.node);
 
   const [selectedVariant, setSelectedVariant] = useState(
-      variants.length > 0 ? variants[0] : null
-    );
+    variants.length > 0 ? variants[0] : null
+  );
 
-    const handleVariantChange = (event) => {
-      const variant = variants.find((v) => v.id === event.target.value);
-      setSelectedVariant(variant);
-    };
+  const handleVariantChange = (event) => {
+    const variant = variants.find((v) => v.id === event.target.value);
+    setSelectedVariant(variant);
+  };
 
   return (
-    <div
-      key={product.id}
-      className="uniq-prod-for-col"
-    >
-      <img
-        src={product.featuredImage.url}
-        alt={product.featuredImage.altText || product.title}
-        className="w-[200px] aspect-square mx-auto cursor-pointer"
-        onClick={() => navigate(`/allcollections/${collectionHandle}/${product.handle}`)}
-      />
+    <div key={product.id} className="uniq-prod-for-col">
+      <div className="aspect-square mx-auto cursor-pointer">
+        <img
+          src={product.featuredImage.url}
+          alt={product.featuredImage.altText || product.title}
+          width={"100%"}
+          height={"100%"}
+          onClick={() =>
+            navigate(`/allcollections/${collectionHandle}/${product.handle}`)
+          }
+        />
+      </div>
       <h3
         className="mt-2 font-semibold cursor-pointer"
-        onClick={() => navigate(`/allcollections/${collectionHandle}/${product.handle}`)}
+        onClick={() =>
+          navigate(`/allcollections/${collectionHandle}/${product.handle}`)
+        }
       >
         {product.title}
       </h3>
       <p className="text-gray-600">
         Rs. {selectedVariant ? selectedVariant.price.amount : "N/A"}
       </p>
-      {variants.length > 1 && ( 
+      {variants.length > 1 && (
         <select
           className="border p-2 mt-2 rounded w-full"
           onChange={handleVariantChange}
