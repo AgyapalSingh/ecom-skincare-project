43684b0ef52901107d082d209cc1ae8ce48b6284
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-02-18 16:13:28 +0530

    Loadmore button added on allProducts collection

--- src/components/collections/AllProducts.jsx
+++ src/components/collections/AllProducts.jsx
@@ -7,8 +7,14 @@ import ProductCard from "../snippets/ProductCard";
 
 const AllProducts = () => {
   const [loading, setLoading] = useState(true);
-  const { cart, addToCart } = useCart();
+  const { addToCart } = useCart();
   const [products, setProducts] = useState([]);
+
+  const [visibleCount, setVisibleCount] = useState(6);
+
+  const loadMore = () => {
+    setVisibleCount((prevCount) => prevCount + 6);
+  };
 
   const fetchProducts = async () => {
     const query = { query: GET_PRODUCTS };
@@ -33,13 +39,25 @@ const AllProducts = () => {
         <UniqayaLoader />
       ) : (
         <section className="max-w-[1130px] mx-auto px-4 sm:px-6 lg:px-0 justify-self-center">
-          <h2 className="text-2xl font-bold text-center my-8">All Products</h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
-            {products.map(({ node }) => (
-              <ProductCard key={node.id} product={node} addToCart={addToCart} />
-            ))}
-          </div>
-        </section>
+      <h2 className="text-2xl font-bold text-center my-8">All Products</h2>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+        {products.slice(0, visibleCount).map(({ node }) => (
+          <ProductCard key={node.id} product={node} addToCart={addToCart} />
+        ))}
+      </div>
+
+      {visibleCount < products.length && (
+        <div className="text-center mt-8">
+          <button
+            onClick={loadMore}
+            className="bg-blue-600 text-white px-6 py-3 rounded hover:bg-blue-700 transition duration-300"
+          >
+            Load More
+          </button>
+        </div>
+      )}
+    </section>
       )}
     </>
   );
