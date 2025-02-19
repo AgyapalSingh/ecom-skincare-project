1092687c86cce6347ae4be8abedc5b6d4f408a66
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-02-19 16:25:23 +0530

    Renaming ProductCard.css

--- src/components/snippets/Cards/ProductCard.jsx
+++ src/components/snippets/Cards/ProductCard.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useCart } from "../../../context/CartContext";
+import "../snippetsCss/ProductCard.css"
 
 const ProductCard = ({ product }) => {
   const navigate = useNavigate();
