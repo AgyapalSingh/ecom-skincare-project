298cf80a6bf961fc3063facf1fddf14c9abe3904
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-02-20 10:47:59 +0530

    Header CSS Updated

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -20,79 +20,49 @@ const Header = () => {
   };
 
   return (
-    <header className="h-28 bg-[#183457] text-amber-50 justify-center z-10 px-4 fixed top-0 right-0 left-0">
-      <AnnounceMentBar />
+    <header className="uniq-header">
+      <div className="uniq-announementBar">
+        <AnnounceMentBar />
+      </div>
 
-      <nav className="container mx-auto flex items-center justify-between  justify-self-center px-4">
-        <div className="flex items-center">
-          <div className="text-3xl font-semibold text-white">
-            <Link to="/" className="navbar-brand">
-              <img
-                alt="Uniqaya-Logo"
-                width={"100%"}
-                height={"100%"}
-                className=" h-8"
-                src="https://cdn.shopify.com/s/files/1/0589/0192/1956/files/Uniqaya_Logo_Wide_Clean_Functional.png?v=1724408288"
-              />
-            </Link>
-          </div>
+      <nav className="uniq-navbar  ">
+        <div className="uniq-navbar-logo">
+          <Link to="/">
+            <img
+              alt="Uniqaya-Logo"
+              width={"150px"}
+              height={"100%"}
+              src="https://cdn.shopify.com/s/files/1/0589/0192/1956/files/Uniqaya_Logo_Wide_Clean_Functional.png?v=1724408288"
+            />
+          </Link>
         </div>
 
-        <div>
-          <div className="mx-4">
-            <ul className="flex flex-row">
-              <li>
-                <NavLink
-                  className="nav-link dropdown-toggle"
-                  to={"/allcollections"}
-                  data-bs-toggle="dropdown"
-                >
-                  All Collections
-                </NavLink>
-              </li>
+        <div className="uniq-navbar-menus">
+          <ul className="uniq-navbar-menus-list">
+            <li>
+              <NavLink to={"/allcollections"}>All Collections</NavLink>
+            </li>
 
-              <li>
-                <NavLink
-                  className="nav-link dropdown-toggle"
-                  to={"/products"}
-                  data-bs-toggle="dropdown"
-                >
-                  All Products
-                </NavLink>
-              </li>
+            <li>
+              <NavLink to={"/products"}>All Products</NavLink>
+            </li>
 
-              <li>
-                <NavLink
-                  className="nav-link dropdown-toggle"
-                  to={"/allcollections/bestsellers"}
-                  data-bs-toggle="dropdown"
-                >
-                  Best Sellers
-                </NavLink>
-              </li>
+            <li>
+              <NavLink to={"/allcollections/bestsellers"}>Best Sellers</NavLink>
+            </li>
 
-              <li>
-                <NavLink
-                  className="nav-link dropdown-toggle"
-                  to={"/blogs"}
-                  data-bs-toggle="dropdown"
-                >
-                  Blogs
-                </NavLink>
-              </li>
-            </ul>
-          </div>
+            <li>
+              <NavLink to={"/blogs"}>Blogs</NavLink>
+            </li>
+          </ul>
         </div>
 
-        <div className="flex items-center">
-          <div
-            className="relative mx-4 cursor-pointer"
-            onClick={() => setIsSearchOpen(true)}
-          >
+        <div className="uniq-navbar-btns">
+          <div onClick={() => setIsSearchOpen(true)}>
             <LuSearch className="text-white text-5xl" />
           </div>
 
-          <div className="relative mx-4 cursor-pointer">
+          <div>
             <LuUserRound className="text-white text-5xl" />
           </div>
 
