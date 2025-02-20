f4d452b20c3a20b5ccc117cd836778c755a5070d
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-02-20 11:43:35 +0530

    Hamburger Added in Header for Qptimizing Mobile Screen

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -5,19 +5,20 @@ import AnnounceMentBar from "../snippets/AnnounceMentBar";
 import CartDrawer from "./CartDrawer";
 import SearchDrawer from "./SearchDrawer";
 import { PiShoppingCartLight } from "react-icons/pi";
-import { LuSearch } from "react-icons/lu";
-import { LuUserRound } from "react-icons/lu";
+import { LuSearch, LuUserRound } from "react-icons/lu";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdClose } from "react-icons/io";
 
 const Header = () => {
   const { cart } = useCart();
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const totalItems =
     cart.length > 0 ? cart.reduce((acc, item) => acc + item.quantity, 0) : 0;
-  const toggleDrawer = () => {
-    setIsDrawerOpen(!isDrawerOpen);
-  };
+  const toggleDrawer = () => setIsDrawerOpen(!isDrawerOpen);
+  const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
 
   return (
     <header className="uniq-header">
@@ -25,61 +26,85 @@ const Header = () => {
         <AnnounceMentBar />
       </div>
 
-      <nav className="uniq-navbar  ">
+      <nav className="uniq-navbar ">
         <div className="uniq-navbar-logo">
           <Link to="/">
             <img
               alt="Uniqaya-Logo"
-              width={"150px"}
-              height={"100%"}
+              width={150}
+              height={50}
               src="https://cdn.shopify.com/s/files/1/0589/0192/1956/files/Uniqaya_Logo_Wide_Clean_Functional.png?v=1724408288"
             />
           </Link>
         </div>
 
         <div className="uniq-navbar-menus">
-          <ul className="uniq-navbar-menus-list">
+          <ul className="uniq-navbar-menus-list ">
             <li>
-              <NavLink to={"/allcollections"}>All Collections</NavLink>
+              <NavLink to="/allcollections">All Collections</NavLink>
             </li>
-
             <li>
-              <NavLink to={"/products"}>All Products</NavLink>
+              <NavLink to="/products">All Products</NavLink>
             </li>
-
             <li>
-              <NavLink to={"/allcollections/bestsellers"}>Best Sellers</NavLink>
+              <NavLink to="/allcollections/bestsellers">Best Sellers</NavLink>
             </li>
-
             <li>
-              <NavLink to={"/blogs"}>Blogs</NavLink>
+              <NavLink to="/blogs">Blogs</NavLink>
             </li>
           </ul>
         </div>
 
-        <div className="uniq-navbar-btns">
+        <div className="uniq-navbar-btns ">
           <div onClick={() => setIsSearchOpen(true)}>
-            <LuSearch className="text-white text-5xl" />
+            <LuSearch className="text-3xl" />
           </div>
-
           <div>
-            <LuUserRound className="text-white text-5xl" />
+            <LuUserRound className="text-3xl" />
           </div>
-
-          <div className="relative mx-4 cursor-pointer" onClick={toggleDrawer}>
-            <PiShoppingCartLight className="text-white text-5xl" />
-            <span className="absolute top-0 right-0 bg-red-600 text-white text-xs font-semibold rounded-full w-5 h-5 flex items-center justify-center">
-              {totalItems}
-            </span>
+          <div className="relative cursor-pointer" onClick={toggleDrawer}>
+            <PiShoppingCartLight className="text-3xl" />
+            {totalItems > 0 && (
+              <span className="absolute top-0 right-0 bg-red-600 text-white text-xs font-semibold rounded-full w-5 h-5 flex items-center justify-center">
+                {totalItems}
+              </span>
+            )}
+          </div>
+          <div
+            className="uniq-header-hamburger "
+            onClick={toggleMobileMenu}
+          >
+            {isMobileMenuOpen ? <IoMdClose className="text-3xl" /> : <GiHamburgerMenu className="text-3xl" />}
+            {/* <GiHamburgerMenu className="text-3xl" /> */}
           </div>
         </div>
-        <SearchDrawer
-          isOpen={isSearchOpen}
-          closeDrawer={() => setIsSearchOpen(false)}
-        />
-
-        <CartDrawer isOpen={isDrawerOpen} closeDrawer={toggleDrawer} />
       </nav>
+
+      {/* Mobile Menu Drawer */}
+      {isMobileMenuOpen && (
+        <div className="uniq-mobile-nav-drawer">
+          <ul className="uniq-mobile-nav-drawer-menu-list">
+            <li onClick={toggleMobileMenu}>
+              <NavLink to="/allcollections">All Collections</NavLink>
+            </li>
+            <li onClick={toggleMobileMenu}>
+              <NavLink to="/products">All Products</NavLink>
+            </li>
+            <li onClick={toggleMobileMenu}>
+              <NavLink to="/allcollections/bestsellers">Best Sellers</NavLink>
+            </li>
+            <li onClick={toggleMobileMenu}>
+              <NavLink to="/blogs">Blogs</NavLink>
+            </li>
+          </ul>
+        </div>
+      )}
+
+      <SearchDrawer
+        isOpen={isSearchOpen}
+        closeDrawer={() => setIsSearchOpen(false)}
+      />
+      <CartDrawer isOpen={isDrawerOpen} closeDrawer={toggleDrawer} />
     </header>
   );
 };
