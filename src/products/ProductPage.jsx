fe5eec9d496317fbec202cdf96c45abdf77cd163
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-03-05 16:09:58 +0530

    Cart Open When Add to cart clicked from Facewash  Product Page

--- src/products/ProductPage.jsx
+++ src/products/ProductPage.jsx
@@ -120,6 +120,7 @@ const ProductPage = () => {
             selectedVariant={selectedVariant}
             setSelectedVariant={setSelectedVariant}
             addToCart={addToCart}
+            openCartDrawer={toggleDrawer}
           />
         );
 
