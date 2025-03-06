6f97ce2086157f263c81d2b1ec830b17a0df1b3e
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-03-06 10:05:11 +0530

    Footer Classes Removed

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -10,46 +10,31 @@ const Footer = () => {
             <h1>Support</h1>
             <menu>
               <li>
-                <NavLink
-                  className="nav-link dropdown-toggle"
-                  to={"/about-us"}
-                  data-bs-toggle="dropdown"
-                >
-                  About Us
-                </NavLink>
+                <NavLink to={"/about-us"}>About Us</NavLink>
               </li>
               <li>
-                <NavLink
-                  className="nav-link dropdown-toggle"
-                  to={"/contact-us"}
-                  data-bs-toggle="dropdown"
-                >
-                  Contact Us
-                </NavLink>
+                <NavLink to={"/contact-us"}>Contact Us</NavLink>
               </li>
               <li>
-                <NavLink
-                  className="nav-link dropdown-toggle"
-                  to={"/faqs"}
-                  data-bs-toggle="dropdown"
-                >
-                  FAQs
-                </NavLink>
+                <NavLink to={"/faqs"}>FAQs</NavLink>
               </li>
               <li>
-                <NavLink
-                  className="nav-link dropdown-toggle"
-                  to={"/track-order"}
-                  data-bs-toggle="dropdown"
-                >
-                  Track Order
-                </NavLink>
+                <NavLink to={"/track-order"}>Track Order</NavLink>
               </li>
             </menu>
           </div>
         </div>
         <div className="Developers-Name">
-          <h1>Developed By <a href="https://www.linkedin.com/in/agyapal-singh-020983241/" target="_blank"> Agyapal Singh </a></h1>
+          <h1>
+            Developed By{" "}
+            <a
+              href="https://www.linkedin.com/in/agyapal-singh-020983241/"
+              target="_blank"
+            >
+              {" "}
+              Agyapal Singh{" "}
+            </a>
+          </h1>
           <span>With ❤</span>
         </div>
       </footer>
