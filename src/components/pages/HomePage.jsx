8a4371163376fd072d5391bc05b4533e61d571cb
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-03-24 10:46:12 +0530

    Video Component Created

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -5,11 +5,18 @@ import ImageSlider from "../snippets/ImageSlider";
 import Marquee from "../snippets/Marquee";
 import ScrollingText from "../snippets/ScrollingText";
 import MarqueeEcoIcons from "../snippets/MarqueeEcoIcons";
+import Video from "../snippets/Video";
 
 const HomePage = () => {
   return (
     <Layout title="Home | Uniqaya Lifestyle">
-      <section className="uniq-img-banner-slider-sec"> 
+      <section className="uniq-img-banner-slider-sec">
+        <Video
+          largeVideoSrc="https://cdn.shopify.com/videos/c/o/v/a2b0bd497d784a27b9f0e56a6454ed84.mp4"
+          smallVideoSrc="https://cdn.shopify.com/videos/c/o/v/77da774c8b4944e3be56708e78b8738d.mp4"
+        />
+      </section>
+      <section className="uniq-img-banner-slider-sec">
         <ImageSlider />
       </section>
       <section className="uniq-newlyLaunched-sec uniq-home-container">
