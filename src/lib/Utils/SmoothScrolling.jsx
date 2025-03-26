aa68f1bd01081ac999183b8196856597d3f0b2a2
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-03-26 11:50:37 +0530

    Lenis Options Updated

--- src/lib/Utils/SmoothScrolling.jsx
+++ src/lib/Utils/SmoothScrolling.jsx
@@ -1,7 +1,7 @@
 import { ReactLenis } from "lenis/react";
 function SmoothScrolling({ children }) {
   return (
-    <ReactLenis root options={{ lerp: 0.1, duration: 2, smoothTouch: true }}>
+    <ReactLenis root options={{ lerp: 0.1, duration: 2, smoothTouch: 0.1, syncTouch: true }}>
       {children}
     </ReactLenis>
   );
